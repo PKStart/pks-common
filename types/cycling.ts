38593c4198db819f0d4b177daf872f2a10af6ea0
Peter Kin <kinpeter85@gmail.com>
2022-10-26 19:28:12 +0200

    Update cycling types

--- types/cycling.ts
+++ types/cycling.ts
@@ -9,9 +9,9 @@ export interface CyclingChore {
 
 export interface Cycling extends BaseEntity {
   userId: UUID
-  weeklyGoal: number
-  monthlyGoal: number
-  chores: CyclingChore[]
+  weeklyGoal: number | null
+  monthlyGoal: number | null
+  chores: CyclingChore[] | null
 }
 
 export type SetWeeklyGoalRequest = Pick<Cycling, 'weeklyGoal'>
